9070a7dd9164668380165b1bc49e77d76b1b943e
ArtificialCoder-02 <rc.tuhin02@gmail.com>
2023-09-21 21:21:44 +0530

    Added Map Functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+// Initialize the map
+var map = L.map('map').setView([51.505, -0.09], 13); // Set initial coordinates and zoom level
+
+// Add a tile layer using OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+// Add a satellite imagery (aerial imagery) layer
+var satelliteLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+satelliteLayer.addTo(map);
