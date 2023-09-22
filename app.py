181d0e782beef227686db766d4c4ffaa7ae61946
Tuhin Roychowdhury <tuhinrc@Tuhins-MacBook-Air.local>
2023-09-22 13:12:52 +0530

    Made Changes

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 import os
 import base64
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
 
 app = Flask(__name__)
 
@@ -11,13 +14,13 @@ def index():
 @app.route('/detect-objects', methods=['POST'])
 def detect_objects():
     # Get the snapshot image data from the request
-    snapshot_data = request.form['snapshot']
+    snapshot_data = request.json['snapshot']  # Use request.json to access JSON data
 
     # Convert the base64 data to bytes
     snapshot_bytes = base64.b64decode(snapshot_data)
 
     # Define a path for a temporary image file
-    temp_image_path = 'temp_snapshot.jpg'
+    temp_image_path = 'yolov5/DOTA/val/images/P0003.png'
 
     # Save the image to a temporary file
     with open(temp_image_path, 'wb') as temp_image:
@@ -32,12 +35,40 @@ def detect_objects():
     return jsonify({'detected_objects': detected_objects})
 
 def perform_object_detection(image_path):
-    # This is a placeholder for your actual YOLOv5 detection logic
-    # Replace this with your YOLOv5 detection implementation
-    # You'll read the image from the given image_path and perform object detection
+    # Initialize headless browser (assuming you're using Chrome)
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(options=chrome_options)
+    
+    # Open the HTML file with the map
+    driver.get('http://127.0.0.1:5000/')  # Update with your actual URL
+
+    # Wait for the map to load (you may need to adjust the sleep time)
+    time.sleep(5)
+
+    # Capture a screenshot of the visible portion of the map
+    screenshot_path = 'Outputs/screenshot.png'
+    driver.save_screenshot(screenshot_path)
+    
+    # Close the browser
+    driver.quit()
+
+    # Process the screenshot for object detection
+    detected_objects = process_screenshot_for_detection(screenshot_path)
+
+    # Delete the screenshot file
+    os.remove(screenshot_path)
+
+    return detected_objects
+
+def process_screenshot_for_detection(screenshot_path):
+    # This is a placeholder for processing the screenshot for object detection
+    # Replace this with your actual processing logic
+    # You'll read the screenshot image and perform object detection
     # Return a list of detected objects
     # For now, we'll return a hardcoded list of objects for testing
     return ['object1', 'object2']
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -25,3 +25,76 @@ const searchControl = L.Control.geocoder({
     map.setView(e.geocode.center, 15);
     // You can do something with the geocode result here if needed
 }).addTo(map);
+
+function captureMapSnapshot() {
+    console.log('Button clicked!');  // Log a message when the button is clicked
+
+    const mapElement = document.getElementById('map');
+    
+    html2canvas(mapElement, {
+        scrollX: 0,
+        scrollY: 0,
+        width: mapElement.offsetWidth,
+        height: mapElement.offsetHeight,
+        logging: false,
+        onclone: (doc) => {
+            // You can customize the cloned document if needed
+        }
+    }).then((canvas) => {
+        const snapshotData = canvas.toDataURL('image/jpeg');
+
+        // Send the snapshot data to the server for object detection
+        sendSnapshotForDetection(snapshotData);
+    });
+}
+
+function sendSnapshotForDetection(snapshotData) {
+    fetch('/detect-objects', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            snapshot: snapshotData
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Detected objects:', data.detected_objects);
+
+        // Call a function to display detected objects on the map
+        displayDetectedObjects(data.detected_objects);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+// Function to display detected objects on the map
+// Function to display detected objects on the map
+function displayDetectedObjects(detectedObjects) {
+    // Clear any previous displayed rectangles
+    map.eachLayer(layer => {
+        if (layer instanceof L.Rectangle) {
+            map.removeLayer(layer);
+        }
+    });
+
+    // Loop through detected objects and display rectangles
+    detectedObjects.forEach(object => {
+        const bounds = object.bbox; // Assuming object.bbox contains [minX, minY, maxX, maxY]
+
+        const rectangle = L.rectangle([[bounds[1], bounds[0]], [bounds[3], bounds[2]]], {
+            color: 'blue',
+            weight: 2,
+            opacity: 0.7,
+            fillOpacity: 0.1
+        });
+
+        rectangle.addTo(map);
+    });
+}
+const detectButton = document.getElementById('detectButton');
+detectButton.addEventListener('click', () => {
+    captureMapSnapshot();
+});
