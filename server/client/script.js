200b183df0d3ee6e5206adf5b8f6f8bdf1e95be4
artificialcoder02 <rc.tuhin02@gmail.com>
2023-10-30 12:49:36 +0530

    Added Support for Leaflet| Removed Mapbox Functionalities

--- server/client/script.js
+++ server/client/script.js
@@ -13,9 +13,9 @@ document.querySelector('.pop-button').addEventListener('click', function () {
 });
 
 // Add click event listener to the capture button
-document.querySelector('.close-button').addEventListener('click', function () {
-    showPopup();
-});
+//document.querySelector('.close-button').addEventListener('click', function () {
+    //showPopup();
+//});
 function showPopup() {
     document.getElementById('popup').style.display = 'flex';
 }
@@ -23,8 +23,6 @@ function showPopup() {
 function closePopup() {
     document.getElementById('popup').style.display = 'none';
 }
-
-
 
 async function save() {
     const imageFile = document.getElementById('imageUpload').files[0];
@@ -85,7 +83,9 @@ async function save() {
         console.error('Error:', error);
         alert('An error occurred while saving files on the server.');
     }
+
 }
+
 function updateImageLabel() {
     const input = document.getElementById('imageUpload');
     const label = document.getElementById('imageLabel');
@@ -121,22 +121,24 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYXNoaXNoMDYxMyIsImEiOiJjbG51NndoZnYwYnN3MnFucDkwNHFkYms4In0.KfM4hlN5C1-aCEe8JVfEoQ';
-    map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/ashish0613/cln332wie038l01qx0shq9ees',
-        center: [-0.454295, 51.470020],
-        zoom: 18
-    });
+    // Create a Leaflet map and set the initial view
+    map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 18);
 
-    new mapboxgl.Marker().setLngLat([2.349014, 48.864716]).addTo(map);
+    L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        crossOrigin : true,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    }).addTo(map);
 
-    // Add click event listener to the capture button
-    document.querySelector('.capture-button').addEventListener('click', function () {
-        captureMapAndSave();
-    });
+    // Add a Leaflet marker at the user's location
+    L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+    L.control.bigImage({position: 'topright'}).addTo(map);
 }
 
+// Add click event listener to the "Detect Objects" button
+document.getElementById('detectButton').addEventListener('click', function () {
+    captureMapAndSave();
+});
 
 function captureMapAndSave() {
     // Show the loading spinner and hide the button
@@ -144,78 +146,69 @@ function captureMapAndSave() {
     const detectButton = document.getElementById('detectButton');
     loadingSpinner.style.display = 'block';
     detectButton.style.display = 'none';
-    // Use Mapbox's screenshot functionality
-    map.once('render', function () {
-        map.getCanvas().toBlob(function (blob) {
-            // Convert the blob to a base64 data URL
-            const reader = new FileReader();
-            reader.readAsDataURL(blob);
-            reader.onloadend = function () {
-                const base64Data = reader.result;
 
-                // Create a download link for the captured image
-                const url = URL.createObjectURL(blob);
-                const a = document.createElement('a');
-                a.href = url;
-                a.download = 'map_capture.png';
-                document.body.appendChild(a);
+    // Capture the Leaflet map using html2canvas
+    html2canvas(map.getContainer()).then(canvas => {
+        // Convert the canvas to a base64 data URL
+        const base64Data = canvas.toDataURL('image/png');
 
-                // Send the base64Data to the server using an HTTP POST request
-                fetch('/save-captured-image', {
-                    method: 'POST',
-                    body: JSON.stringify({ image: base64Data }),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }).then((response) => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json(); // Parse the response body as JSON
-                })
-                    .then((data) => {
-                        // Hide the loading spinner and show the button again
-                        loadingSpinner.style.display = 'none';
-                        detectButton.style.display = 'block';
-                        // Now, "data" should contain the processedImage as a base64 string
-                        map.getContainer().style.display = 'none';
-                        // Create the imgElement within the "upper_box" div
-                        // Create the imgElement within the "image" div
-                        const imgElement = document.createElement('img');
-                        imgElement.src = 'data:image/png;base64,' + data.processedImage;
+        // Create a download link for the captured image
+        const a = document.createElement('a');
+        a.href = base64Data;
+        a.download = 'map_capture.png';
+        document.body.appendChild(a);
 
-                        // Get the "image" div by its id and append the imgElement to it
-                        const imageDiv = document.getElementById('image');
-                        imageDiv.style.display = 'block';
-                        imageDiv.appendChild(imgElement);
+        // Send the base64Data to the server using an HTTP POST request
+        fetch('/save-captured-image', {
+            method: 'POST',
+            body: JSON.stringify({ image: base64Data }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // Parse the response body as JSON
+        }).then(data => {
+            // Hide the loading spinner and show the button again
+            loadingSpinner.style.display = 'none';
+            detectButton.style.display = 'block';
+            // Now, "data" should contain the processedImage as a base64 string
+            map.getContainer().style.display = 'none';
+            // Create the imgElement within the "upper_box" div
+            const imgElement = document.createElement('img');
+            imgElement.src = 'data:image/png;base64,' + data.processedImage;
 
-                    })
-                    .catch((error) => {
-                        console.error('Error:', error);
-                        alert('Failed to save files on the server.');
-                        loadingSpinner.style.display = 'none';
-                        detectButton.style.display = 'block';
-                    });
-            };
-        }, 'image/png');
+            // Get the "image" div by its id and append the imgElement to it
+            const imageDiv = document.getElementById('image');
+            imageDiv.style.display = 'block';
+            imageDiv.appendChild(imgElement);
+        }).catch(error => {
+            console.error('Error:', error);
+            alert('Failed to save files on the server.');
+            loadingSpinner.style.display = 'none';
+            detectButton.style.display = 'block';
+        });
     });
-
-    const imageDiv = document.getElementById('image');
-
-    imageDiv.addEventListener('dblclick', function () {
-        // Check if there is an <img> element within the "image" div
-        const imgElement = imageDiv.querySelector('img');
-
-        if (imgElement) {
-            // If an <img> element exists, remove it
-            imgElement.remove();
-        }
-
-        // Always show the map container after double-click
-        map.getContainer().style.display = 'block';
-
-        // Hide the "image" div
-        imageDiv.style.display = 'none';
-    });
-    map.triggerRepaint();
 }
+
+const imageDiv = document.getElementById('image');
+
+imageDiv.addEventListener('dblclick', function () {
+    // Check if there is an <img> element within the "image" div
+    const imgElement = imageDiv.querySelector('img');
+
+    if (imgElement) {
+        // If an <img> element exists, remove it
+        imgElement.remove();
+    }
+
+    // Always show the map container after double-click
+    map.getContainer().style.display = 'block';
+
+    // Hide the "image" div
+    imageDiv.style.display = 'none';
+});
+
+//map.clearLayers();
